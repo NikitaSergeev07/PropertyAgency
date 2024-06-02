87b563bfa34cbf9f98bf22fbb12609c77ae0f0d6
Kirill <y.kosarev@idaproject.com>
2024-06-02 16:55:48 +0300

    Добавил интерфейсы для properties

---
+++ frontend/assets/types/property.ts
@@ -0,0 +1,38 @@
+export interface PropertyAddress {
+    id: string;
+    street: string;
+    city: string;
+    state: string;
+    country: string;
+    zipCode: string;
+    propertyId: string;
+}
+
+export interface Favorites {
+    id: string;
+    userId: string;
+    user: string;
+    propertyId: string;
+}
+
+export interface Property {
+    id: string;
+    title: string;
+    description: string;
+    price: number;
+    roomCount: number;
+    status: string;
+    userId: string;
+    user: {
+        id: string;
+        userName: string;
+        email: string;
+        role: number;
+        properties: any[];
+        favorites: Favorites[],
+        rentals: any[]
+    }
+    address: PropertyAddress;
+    favorites: Favorites[];
+    rentals: any[];
+}
