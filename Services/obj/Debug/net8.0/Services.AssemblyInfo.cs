dc2523e6f8273a6031ea6a28b99f8ec634bb4730
Stupin E. <89169177436@mail.ru>
2024-06-03 00:56:41 +0300

    m

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39604fda5298c84780230e6d87a2cec1ab92ede4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab1e437b1f169192bb06482715366b4c61d338c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
